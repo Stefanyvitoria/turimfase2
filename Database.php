d0ec897dfe3a03b8812f2c45af8f4125cb757be6
Stefany Izidio <Izidiostefany@gmail.com>
2022-08-17 12:35:35 -0300

    Commit inicial do front-end.

---
+++ Database.php
@@ -0,0 +1,19 @@
+<?php
+
+    class bd {
+        
+        public function criarBD($host, $user, $password, $database) {
+        }
+
+        public function conectar($host, $user, $password, $database) {
+        }
+
+        public function gravar($json) {
+        }
+
+        public function ler() {
+        }
+    }
+
+
+?>
